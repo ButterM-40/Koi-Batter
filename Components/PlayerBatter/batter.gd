1e6164be75233b8677ac199af320b26bab11430a
roburab <166461346+roburab@users.noreply.github.com>
2026-07-12 04:39:43 +0200

    player sprite

--- Components/PlayerBatter/batter.gd
+++ Components/PlayerBatter/batter.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 
 @export var animation_player : AnimatedSprite2D
 
+@onready var animationplayer = $AnimationPlayer 
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -14,6 +17,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 	if Input.is_action_just_pressed("Hit"):
 		action_hit()
 
@@ -22,11 +26,24 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			animationplayer.play("run")
+		else:
+			animationplayer.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			animationplayer.play("stand")
+		else:
+			animationplayer.play("jump")
 
 	move_and_slide()
 
-func action_hit() -> void:
+	if direction < 0:
+		animationplayer.flip_h = true
+	if direction > 0:
+		animationplayer.flip_h = false
 	
-	pass
+func action_hit() -> void:
+	animationplayer.play("hit")
+	
